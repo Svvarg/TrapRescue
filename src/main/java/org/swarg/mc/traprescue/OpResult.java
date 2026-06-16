4e435a67d27e7ddb18a31ae0ccadd0331e172e4f
svvarg <54898192+Svvarg@users.noreply.github.com>
2026-06-16 14:04:03 +0300

    feat: add playload to OpResult

--- src/main/java/org/swarg/mc/traprescue/OpResult.java
+++ src/main/java/org/swarg/mc/traprescue/OpResult.java
@@ -5,14 +5,35 @@ package org.swarg.mc.traprescue;
 public class OpResult {
     public final boolean success;
     public final String message;
+    public final Object data;
 
     public OpResult(boolean success, String message) {
         this.success = success;
         this.message = message;
+        this.data = null;
+    }
+
+    public OpResult(boolean success, String message, Object data) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
     }
 
     public boolean isSuccess() { return success; }
 
     public static OpResult ok(String msg) { return new OpResult(true, msg); }
     public static OpResult fail(String msg) { return new OpResult(false, msg); }
+
+    public static OpResult okData(Object data) {
+        return new OpResult(true, "", data);
+    }
+
+    public boolean isSuccessWithData(Class<?> cls) {
+        return success && data != null && cls != null && cls.isInstance(data);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T getData(Class<T> clazz) {
+        return isSuccessWithData(clazz) ? (T) data : null;
+    }
 }
